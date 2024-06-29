f91754332099bbccc552af5147dea207d22b0850
Leon Colaco <leon.colaco@student.saedubai.com>
2024-06-29 16:39:26 +0400

    enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,16 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-{
-    // Start is called before the first frame update
+{ 
+
+    [SerializeField] Vector3 startPoint;
+    [SerializeField] Vector3 endPoint;
+    [SerializeField] Vector3 normalized;
+    [SerializeField] Vector3 final;
+    
+    float x;
+    float y;
+    float m;
+
+    float distance;
+    float overcame = 0;
     void Start()
     {
-        
+        final = startPoint;
+
+        normalized.x = endPoint.x - startPoint.x;
+        normalized.y = endPoint.y - startPoint.y;     
+
+        x = normalized.x * normalized.x;
+        y = normalized.y * normalized.y;
+
+        m = Mathf.Sqrt(x + y);
+
+        normalized.x = normalized.x / m;
+        normalized.y = normalized.y / m;
+
+
+        distance = Vector3.Distance(startPoint, endPoint);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (overcame <= distance)
+        {
+            overcame += normalized.magnitude;
+            final += normalized;
+        }
+        else
+        {
+            final = endPoint;
+        }
+
+     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(startPoint, endPoint);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(startPoint, final);
     }
 }
