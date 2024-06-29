12c836d96d942c5751e7ec4e1fe3c7e26ffcd0d5
Leon Colaco <leon.colaco@student.saedubai.com>
2024-06-29 20:17:34 +0400

    few changes for the scene and characters

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,59 +4,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform location;
-    public Player player;
-    Vector3 startPoint;
-    Vector3 endPoint;
-    Vector3 normalized;
-    Vector3 final;
-    
-    float x;
-    float y;
-    float m;
-
-    float distance;
-    float overcame = 0;
+    public Transform player;
+    public GameObject enemy;
     void Start()
     {
-        player = GetComponent<Player>();
-        startPoint = player.transform.position;
-        endPoint = gameObject.transform.position;
-
-        normalized.x = endPoint.x - startPoint.x;
-        normalized.y = endPoint.y - startPoint.y;     
-
-        x = normalized.x * normalized.x;
-        y = normalized.y * normalized.y;
-
-        m = Mathf.Sqrt(x + y);
-
-        normalized.x = normalized.x / m;
-        normalized.y = normalized.y / m;
-
-
-        distance = Vector3.Distance(startPoint, endPoint);
+        Vector2 enemypostion;
     }
 
     void Update()
     {
-        if (overcame <= distance)
-        {
-            overcame += normalized.magnitude;
-            final += normalized;
-        }
-        else
-        {
-            final = endPoint;
-        }
+        
 
-     }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(startPoint, endPoint);
-
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(startPoint, final);
     }
+
 }
