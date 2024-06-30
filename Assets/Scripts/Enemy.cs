986639e147e97a2f9bff85ed7231b61d04e1765a
Leon Colaco <leon.colaco@student.saedubai.com>
2024-06-30 21:12:35 +0400

    New scene to show end and play again button

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform player;
-    public GameObject enemy;
+    [SerializeField] private Transform target;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    //Find player's position
     void Start()
     {
-        Vector2 enemypostion;
+        target = FindObjectOfType<Player>().transform;
     }
 
     void Update()
     {
-        
+        var sub = target.position - transform.position;
 
+        transform.up = Vector3.MoveTowards(transform.up, sub , rotationSpeed * Time.deltaTime);
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.up, moveSpeed * Time.deltaTime);
+
+        Deathchecker();
     }
 
+
+    //action when enemy near enough to player
+    public virtual void Deathchecker()
+    {
+        if (Vector3.Distance(transform.position, target.position) <= 2)
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(1);
+        }
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //[SerializeField] private int health = 5;
     [SerializeField] private float speed = 5.0f;
     [SerializeField] public Vector2 playerLocation;
-
+    protected new string name;
     Vector2 movement;
 
     public Rigidbody2D rb;
 
     void Update()
     {
+        // name of the player
+        name = "Travis";
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
